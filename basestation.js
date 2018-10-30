d67eb307d899ea494fdc7d728a1859f6e417c2be
davideeparry <davideeparry@gmail.com>
2018-10-29 17:04:00 -0700

    Seperating the version that will talk to the boat from one used for testing the db

--- basestation.js
+++ basestation.js
@@ -1,5 +1,16 @@
 const WebSocket = require('ws');
 const parseJson = require('parse-json');
+var SerialPort = require('serialport');
+var Readline = require('@serialport/parser-readline'); // nice little parser that uses a newline delimiter
+var parser = new Readline();
+var port = new SerialPort('COM3', function(err) {
+    if (err) {
+        console.log('Error opening serial port: ', err.message);
+        // will need callback wrappers for reconnecting
+    }
+});
+
+port.pipe(parser);
 
 const wss = new WebSocket.Server({
     port: 8080
@@ -9,12 +20,18 @@ wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
         var msg = JSON.parse(message);
         if (msg.hasOwnProperty('uuid')) {
-            ws.send(message);
-            console.log(message);
+            port.write(message, function(err) {
+                if (err) {
+                    console.log("Error writing to serial: ", err.message);
+                }
+            });
         } else {
             console.log("Junk message");
             // Need more error logging here
         }
+    });
+    parser.on('data', function (msgFromBoat) {
+        ws.send(messageFromBoat);
     });
 });
 
